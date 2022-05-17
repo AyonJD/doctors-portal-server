c4ba2266d0f431f415ac2fc1103ba074cecb1cff
Ayon <ayonjodder177@gmail.com>
2022-05-17 09:04:51 +0600

    Service api done

--- index.js
+++ index.js
@@ -11,7 +11,26 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pqhyp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+console.log(uri);
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+        console.log('Database connected');
 
+        //getting all services
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally {
+        // await client.close()
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('From Doctors portal')
